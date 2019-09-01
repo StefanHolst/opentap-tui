4431984c281fab456818473c242b98439581044a
Stefan Holst <stefanholst0@gmail.com>
2019-09-02 00:06:18 +0200

    Removed unused usings

--- OpenTAP.TUI/OpenTAP.TUI.cs
+++ OpenTAP.TUI/OpenTAP.TUI.cs
@@ -1,6 +1,5 @@
 ﻿using NStack;
 using OpenTap;
-using OpenTap.Diagnostic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,8 +8,6 @@ using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 using Terminal.Gui;
 
