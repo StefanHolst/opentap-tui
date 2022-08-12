aa35019c7451806897a7f9d6977ceef19a9ca498
Frederik Andreasen <frederik.andreasen@keysight.com>
2022-08-12 12:58:34 +0200

    Fix TUI crash on file accessed by another process

--- OpenTAP.TUI/Views/TestPlanView.cs
+++ OpenTAP.TUI/Views/TestPlanView.cs
@@ -168,8 +168,15 @@ namespace OpenTap.Tui.Views
 
             if (string.IsNullOrWhiteSpace(path) == false)
             {
-                Plan.Save(path);
-                TUI.Log.Info($"Saved test plan to '{Plan.Path}'.");
+                try
+                {
+                    Plan.Save(path);
+                    TUI.Log.Info($"Saved test plan to '{Plan.Path}'.");
+                }
+                catch (IOException ex)
+                {
+                    TUI.Log.Error(ex);
+                }
             }
         }
         
